b900f1579705fa1e3f9e6bb94084492619df3344
Ernest Lin <ernestl8502@gmail.com>
2022-02-09 17:19:40 -0800

    Fixed state abbrev. bug.

--- Collar_Cleaning_code.py
+++ Collar_Cleaning_code.py
@@ -1,8 +1,69 @@
 import pandas as pd
 
+#Copied from https://gist.github.com/rogerallen/1583593
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+
 def clean_labor_data():
     #Read excel file, renames first column to States and take out null rows
-    raw_labor_data = pd.read_excel("/Raw_Data/collar_dataset_raw.xlsx", header = 4)
+    raw_labor_data = pd.read_excel("./Raw_Data/collar_dataset_raw.xlsx", header = 4)
     raw_labor_data.rename(columns={"Unnamed: 0": "State"}, inplace=True)
     raw_labor_data = raw_labor_data.dropna()
 
@@ -18,13 +79,13 @@ def clean_labor_data():
     #Reset index to start at 0
     raw_labor_data = raw_labor_data.reset_index(drop = True)
 
-    #Convert state names into codes (First two letters of each state name)
-    raw_labor_data["State"] = raw_labor_data["State"].apply(lambda state_name: state_name[:2].upper())
-
     #eliminated extra characters in state names
     raw_labor_data["State"] = raw_labor_data["State"].str.replace('\d+', '')
     raw_labor_data["State"] = raw_labor_data["State"].str.replace('(', '')
     raw_labor_data["State"] = raw_labor_data["State"].str.replace(')', '')
+
+    #Convert state names into codes (First two letters of each state name)
+    raw_labor_data["State"] = raw_labor_data["State"].apply(lambda state_name: us_state_to_abbrev[state_name])
 
     #Original raw data has different columns stacked on top of each row, so we need to reorder the dataset.
     #Block 1 contains total, constructing and mining data
@@ -50,6 +111,6 @@ def clean_labor_data():
     #Get rid of all other columns except State, White_col, Blue_col and Total
     labor_data.drop(columns = ["Constructing", "Mining", "Trade", "Financial", "Professional", "Education", "Leisure", "Gov"], inplace=True)
     #export as csv
-    labor_data.to_csv('/Cleaned Data/state_labor_data.csv')
+    labor_data.to_csv('./Cleaned Data/state_labor_data.csv')
 
 clean_labor_data()
