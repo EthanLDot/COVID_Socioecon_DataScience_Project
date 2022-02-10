1ae85c56b7559f2cf7bb82a5936635a21d18ee6c
EthanLDot <ethanlee2337@gmail.com>
2022-02-09 16:19:27 -0800

    Merged uncleaned/raw data folders, fixed bug in labor data cleaning, comments

--- Collar_Cleaning_code.py
+++ Collar_Cleaning_code.py
@@ -2,37 +2,45 @@ import pandas as pd
 
 def clean_labor_data():
     #Read excel file, renames first column to States and take out null rows
-    raw_collar = pd.read_excel("Raw_Data/collar_dataset_raw.xlsx", header = 4)
-    raw_collar.rename(columns={"Unnamed: 0": "State"}, inplace=True)
-    raw_collar = raw_collar.dropna()
-
+    raw_labor_data = pd.read_excel("Raw_Data/collar_dataset_raw.xlsx", header = 4)
+    raw_labor_data.rename(columns={"Unnamed: 0": "State"}, inplace=True)
+    raw_labor_data = raw_labor_data.dropna()
+    
     #Take out data from 2021 and only keep 2020
-    raw_collar = raw_collar[["State", "Dec.\n2020", "Dec.\n2020.1", "Dec.\n2020.2"]]
+    raw_labor_data = raw_labor_data[["State", "Dec.\n2020", "Dec.\n2020.1", "Dec.\n2020.2"]]
 
     non_states = ["Virgin Islands", "District of Columbia", "Puerto Rico"]
 
     #Removes all non official states from dataset
     for region in non_states:
-        raw_collar = raw_collar[raw_collar["State"].str.contains(region)==False]
+        raw_labor_data = raw_labor_data[raw_labor_data["State"].str.contains(region)==False]
 
     #Reset index to start at 0
-    raw_collar = raw_collar.reset_index(drop = True)
+    raw_labor_data = raw_labor_data.reset_index(drop = True)
+
+    #eliminated extra characters in state names
+    raw_labor_data["State"] = raw_labor_data["State"].str.replace('\d+', '')
+    raw_labor_data["State"] = raw_labor_data["State"].str.replace('(', '')
+    raw_labor_data["State"] = raw_labor_data["State"].str.replace(')', '')
 
     #Original raw data has different columns stacked on top of each row, so we need to reorder the dataset.
     #Block 1 contains total, constructing and mining data
-    block1 = raw_collar[:50]
+    block1 = raw_labor_data[:50]
     block1.columns = ["State", "Total", "Constructing", "Mining"]
 
     #Block 2 contains Trade, Financial and Professional
-    block2 = raw_collar[50:100]
+    block2 = raw_labor_data[50:100]
     block2.columns = ["State", "Trade", "Financial", "Prof"]
 
     #Block 3 contains Education, Leisure and Government
-    block3 = raw_collar[100:]
+    block3 = raw_labor_data[100:]
     block3.columns = ["State", "Education", "Leisure", "Gov"]
 
-    collar = block1.merge(block2, on="State")
-    collar = collar.merge(block3, on="State")
-    collar.to_csv('Cleaned Data/state_labor_data.csv')
+    #merge all blocks into one dataframe
+    labor_data = block1.merge(block2, on="State")
+    labor_data = labor_data.merge(block3, on="State")
+
+    #export as csv
+    labor_data.to_csv('Cleaned Data/state_labor_data.csv')
 
 clean_labor_data()
